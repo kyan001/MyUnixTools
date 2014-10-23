bc6b02863ce9b27c01a50f37d16d84a25f1eada1
Kyan <ykyankai@gmail.com>
2014-10-23 11:07:42 +0800

    Add pOpen()

--- KyanToolKit_Py.py
+++ KyanToolKit_Py.py
@@ -1,16 +1,20 @@
 ##################################################################
-# Version 1.5
+# Version 1.6
 ##################################################################
 import os
 import sys
 import time
 import getpass;
+import subprocess;
+
 class KyanToolKit_Py(object):
 	def __init__(self,trace_file_="trace.xml"):
 		self.trace_file = trace_file_
 		pass
 	def __del__(self):
 		pass
+
+#--Text Process---------------------------------------------------
 	def banner(self,content_="Well Come"):
 		# char def
 		self.special_char = "#"
@@ -29,6 +33,16 @@ class KyanToolKit_Py(object):
 		banner_border = self.special_char * content_line_lenght
 		return banner_border + '\n' + content_line + '\n' + banner_border
 
+	def info(self, words):
+		print("[INFO] " + words)
+
+	def warn(self, words):
+		print("[WARNING] " + words)
+
+	def err(self, words):
+		print("[ERROR] " + words)
+
+#--System Fucntions-----------------------------------------------
 	def clearScreen(self):
 		if "win32" == sys.platform:
 			os.system('cls')
@@ -42,6 +56,26 @@ class KyanToolKit_Py(object):
 		#PY2# raw_input(input_)
 		input(input_)
 
+	def byeBye(self,input_="See you later."):
+		exit(input_)
+
+	def runCmd(self, words):
+		if len(words) > 80:
+			print(self.breakCommands(words));
+		else:
+			print(self.banner(words));
+		result = os.system(words);
+		self.checkResult(result);
+
+	def pOpen(self, words):
+		print(self.banner(words));
+		result = subprocess.Popen(words);
+
+	def readCmd(self, words):
+		result = os.popen(words).read();
+		return result;
+
+#--Get Information------------------------------------------------
 	def getInput(self,question_,prompt_='> '):
 		print(question_)
 		#PY2# return raw_input(prompt_).strip()
@@ -65,9 +99,7 @@ class KyanToolKit_Py(object):
 			self.err("Please enter a valid choice");
 			return self.getChoice(choices_);
 
-	def getUser(self):
-		return getpass.getuser();
-
+#--Pre-checks---------------------------------------------------
 	def needPlatform(self, expect_platform):
 		print("============ Checking Platform ============");
 		self.info("Required Platform: " + expect_platform);
@@ -86,9 +118,7 @@ class KyanToolKit_Py(object):
 		else:
 			self.info("Done\n");
 
-	def byeBye(self,input_="See you later."):
-		exit(input_)
-
+#--Debug---------------------------------------------------------
 	def TRACE(self,input_,trace_type='INFO'):
 		trace_content = ''.join(input_)
 		current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
@@ -104,33 +134,17 @@ class KyanToolKit_Py(object):
 		trace = open(self.trace_file,'a')
 		trace.write(trace_header + trace_content + "\n</" + trace_type + ">\n")
 
-	def runCmd(self, words):
-		if len(words) > 80:
-			def breakCommands(cmd):
-				formatted_cmd = cmd.replace(' -','\n# \t-');
-				formatted_cmd = "##########################.\n# " + formatted_cmd + "\n##########################.";
-				return formatted_cmd;
-			print(breakCommands(words));
-		else:
-			print(self.banner(words));
-		result = os.system(words);
-		self.checkResult(result);
-
-	def readCmd(self, words):
-		result = os.popen(words).read();
-		return result;
-
+#--Internal Uses-------------------------------------------------
 	def checkResult(self, result):
 		if 0 == result:
 			self.info("Done\n")
 		else:
 			self.warn("Failed\n")
 
-	def info(self, words):
-		print("[INFO] " + words)
+	def breakCommands(cmd):
+		formatted_cmd = cmd.replace(' -','\n# \t-');
+		formatted_cmd = "##########################.\n# " + formatted_cmd + "\n##########################.";
+		return formatted_cmd;
 
-	def warn(self, words):
-		print("[WARNING] " + words)
-
-	def err(self, words):
-		print("[ERROR] " + words)
+	def getUser(self):
+		return getpass.getuser();
