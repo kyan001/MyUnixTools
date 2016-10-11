784101c9a68dbb3c494dfb32fffc096d31e00655
Kyan <ykyankai@gmail.com>
2016-10-12 01:06:59 +0800

    refine updatevimrc

--- UpdateVimrc.py
+++ UpdateVimrc.py
@@ -1,77 +1,43 @@
 #!/usr/bin/python3
-#
-# ================================================================
-# UpdateVimrc.py
-#   update users /home/{user}/.vimrc
-#
-# HISTORY
-# ----------------------------------------------------------------
-#     DATE    |     AUTHOR     |  VERSION | COMMENT
-# ------------+----------------+----------+-----------------------
-#  2014-05-08 |     YAN Kai    |   V1.0   | Script Creation
-#             |                |          |
-# ----------------------------------------------------------------
-# ================================================================
-#
 import os
-import sys
-import getpass
-
+import shutil
 import consoleiotools as cit
-import KyanToolKit
-ktk = KyanToolKit.KyanToolKit()
-user = getpass.getuser()
 
-# -Pre-conditions Check-------------------------------------------
-ktk.needPlatform("linux")
-# -get user-------------------------------------------------------
-if len(sys.argv) <= 1:
-    cit.warn("User not passed, using current user.")
-else:
-    user = sys.argv[1]
-vimrc_file = "/home/" + user + "/.vimrc"
-cit.info("User = " + user)
-cit.info("File = " + vimrc_file)
-# -touch file and change owner-----------------------------------
-if not os.path.exists(vimrc_file):
-    cit.info(vimrc_file + " not exists. Create one")
-    ktk.runCmd("touch " + vimrc_file)
-    ktk.runCmd("chown " + user + " " + vimrc_file)
-else:
-    cit.info(vimrc_file + " already exists")
-# -write file----------------------------------------------------
-# \!:1 = 1st argument
-vimrc_template = """\" -----
-\" --Line number
-set nu
-\" --Column number
-set ruler
-\" --ignore case when search
-\"set ic
-\" --tab is 4 spaces long
-\"set ts=4
-\" --highlight search result
-set hls
-\" --highlight line and column
-\"set cuc
-\"set cul
-\" --autoindent and smartindent
-set ai
-set si
-\" --show match brackets
-set sm
-\" --search as you type (incsearch)
-set is
-\" --Show Tab Line(0=no,1=tab>2,2=always)
-set showtabline=1
-\" -----"""
-# vimrc arg def
 
-# file open
-v_file = open(vimrc_file, 'w')
-# replace variables
-final_string = vimrc_template
-# write and close
-v_file.write(final_string)
-v_file.close()
-cit.info("Done. Enjoy.")
+@cit.as_session('Generate Filepath')
+def generate_filepath(filename):
+    home_dir = os.path.expanduser('~')
+    cit.info("Home Dir\t: {}".format(home_dir))
+    if not os.path.isdir(home_dir):
+        cit.warn('Home Dir does not exist, creating it', lvl=1)
+        os.makedirs(home_dir)
+    new_file = os.path.join(home_dir, filename)
+    cit.info("Target File\t: {}".format(new_file))
+    if os.path.exists(new_file):
+        backup = '{}.old'.format(new_file)
+        os.rename(new_file, backup)
+        cit.warn("Target file is already exist.", lvl=1)
+        cit.warn('Old target file renamed as {}'.format(backup), lvl=1)
+    return new_file
+
+
+@cit.as_session('Copying my file')
+def copy_my_file(from_, to_):
+    cit.info('From\t: {}'.format(from_))
+    if not os.path.isfile(from_):
+        cit.err("my.file does not exists, copy cancelled", lvl=1)
+        cit.bye()
+    cit.info('To\t: {}'.format(to_))
+    if os.path.isfile(to_):
+        cit.err('target file exists, copy cancelled', lvl=1)
+        cit.bye()
+    shutil.copyfile(from_, to_)
+    cit.info('Copy done.')
+
+
+def main():
+    new_file = generate_filepath('.vimrc')
+    copy_my_file('my.vimrc', new_file)
+
+if __name__ == '__main__':
+    main()
---
+++ my.vimrc
@@ -0,0 +1,22 @@
+" --Line number
+set nu
+" --Column number
+set ruler
+" --ignore case when search
+"set ic
+" --tab is 4 spaces long
+"set ts=4
+" --highlight search result
+set hls
+" --highlight line and column
+"set cuc
+"set cul
+" --autoindent and smartindent
+set ai
+set si
+" --show match brackets
+set sm
+" --search as you type (incsearch)
+set is
+" --Show Tab Line(0=no,1=tab>2,2=always)
+set showtabline=1
