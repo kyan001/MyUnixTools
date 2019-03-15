8448f368c227ce145973a59b61e32632d9041fda
Kyan <ykyankai@gmail.com>
2019-03-15 19:57:54 +0800

    add compare before backup

--- UpdateConfig.py
+++ UpdateConfig.py
@@ -3,6 +3,15 @@ import os
 import shutil
 import getpass
 import consoleiotools as cit
+import difflib
+
+
+def diff(from_path: str, to_path: str):
+    with open(from_path) as f:
+        from_lines = f.readlines()
+    with open(to_path) as f:
+        to_lines = f.readlines()
+    return list(difflib.unified_diff(from_lines, to_lines))
 
 
 @cit.as_session('Generate Filepath')
@@ -14,36 +23,30 @@ def generate_filepath(filename):
         os.makedirs(home_dir)
     new_file = os.path.join(home_dir, filename)
     cit.info("Target File\t: {}".format(new_file))
-    if os.path.exists(new_file):
-        backup = '{}.old'.format(new_file)
-        os.rename(new_file, backup)
-        cit.warn("Target file is already exist.")
-        cit.warn('Old target file renamed as {}'.format(backup))
     return new_file
 
 
 @cit.as_session('Copying my file')
-def copy_my_file(config_name, to_):
+def copy(from_path: str, to_path: str) -> bool:
     # deal from
-    if not os.path.isabs(config_name):
+    if not os.path.isabs(from_path):
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        from_ = os.path.join(current_dir, config_name)
-    else:
-        from_ = config_name
-    cit.info('From\t: {}'.format(from_))
-    if not os.path.isfile(from_):
+        from_path = os.path.join(current_dir, from_path)
+    cit.info('From\t: {}'.format(from_path))
+    if not os.path.isfile(from_path):
         cit.err("config file does not exists, copy cancelled")
-        cit.bye()
+        return False
     # deal to
-    cit.info('To\t: {}'.format(to_))
-    if os.path.isfile(to_):
+    cit.info('To\t: {}'.format(to_path))
+    if os.path.isfile(to_path):
         cit.err('target file exists, copy cancelled')
-        cit.bye()
+        return False
     cit.info('Copying file ...')
-    shutil.copyfile(from_, to_)
+    shutil.copyfile(from_path, to_path)
     cit.info('Changing file owner ...')
     current_user = getpass.getuser()
-    shutil.chown(to_, user=current_user)
+    shutil.chown(to_path, user=current_user)
+    return True
 
 
 @cit.as_session('Menu')
@@ -53,9 +56,8 @@ def menu():
     choice = cit.get_choice(['ALL'] + confs + ['exit'])
     if choice == 'ALL':
         for conf in confs:
-            config(conf)
+            apply_config(conf)
         cit.info('Done')
-        cit.pause('Press Enter to exit ...')
         cit.bye()
     elif choice == 'exit':
         cit.bye()
@@ -64,15 +66,30 @@ def menu():
 
 
 @cit.as_session('Configing')
-def config(name):
-    new_file = generate_filepath(name)
-    copy_my_file('config' + name, new_file)
+def apply_config(config_name):
+    new_conf = "config" + config_name
+    target_conf = generate_filepath(config_name)
+    if os.path.exists(target_conf):
+        cit.warn("Target file is already exist.")
+        diffs = diff(target_conf, new_conf)
+        if not diffs:
+            cit.warn("Files are same, stop configing.")
+            return True
+        cit.warn("Diffs found:")
+        for diff_line in diffs:
+            cit.warn(diff_line)
+        backup = '{}.old'.format(target_conf)
+        os.rename(target_conf, backup)
+        cit.warn("Old config file renamed as {}".format(backup))
+    return copy(new_conf, target_conf)
 
 
 def main():
     while True:
         conf = menu()
-        config(conf)
+        if not apply_config(conf):
+            cit.bye()
+
 
 if __name__ == '__main__':
     main()
