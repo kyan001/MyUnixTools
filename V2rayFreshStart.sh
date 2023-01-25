14d448691c4d598818a7c7711f76ac9ded674d0d
Kyan <ykyankai@gmail.com>
2023-01-25 17:59:27 +0800

    Update V2rayFreshStart.sh

--- V2rayFreshStart.sh
+++ V2rayFreshStart.sh
@@ -1,17 +1,44 @@
-USERNAME="kyan001"
-if [ "$USER" = "root" ]; then
-    apt install zsh python3-pip  # install apps
+#! /usr/bin/env bash
+echo -n "[?] Please enter your username: "  # do not add \n
+read -r username  # get user raw input
+if [ "$USER" == "root" ]; then
+    echo "[Info] Installing necessary apps ..."
+    apt install -y zsh python3-pip nginx certbot python3-certbot-nginx # install apps, automatically yes.
+    echo "[Info] Cloning MyUnixTools ..."
     git clone https://github.com/kyan001/MyUnixTools  # Clone Git Repo under /root/
     cd MyUnixTools || exit  # /root/MyUnixTools, if failed exit.
-    sh AutoCreateUser.sh -user $USERNAME  # Enter password manually
-    touch /home/$USERNAME/.zshrc  # init zsh
-    chsh -s /bin/zsh $USERNAME  # set zsh as user's shell
-    echo "[Info] You can now relogin using $USERNAME"
+    echo "[Info] Creating unix user ..."
+    sh AutoCreateUser.sh -user $username  # Enter password manually
+    echo "[Info] Initializing zsh ..."
+    touch /home/$username/.zshrc  # init zsh
+    echo "[Info] Set zsh as user default shell"
+    chsh -s /bin/zsh $username  # set zsh as user's shell
+    echo "[Warning] You can now relogin using $username"
     exit
 else
-    git clone https://github.com/kyan001/MyUnixTools  # Clone Git Repo under /home/$USERNAME/
+    # init (p10k config)
+    echo "[Info] Cloning MyUnixTools ..."
+    git clone https://github.com/kyan001/MyUnixTools  # Clone Git Repo under /home/$username/
     cd MyUnixTools || exit  # ~/MyUnixTools, if failed exit.
+    echo "[Info] Setup zsh ..."
     python3 AutoSetupZsh.py
+    echo "[Info] Update user configs ..."
     python3 UpdateConfig.py
-    zsh  # init and exit after
+    echo "[Info] Copy nginx v2ray config file ..."
+    sudo cp config.nginx_v2ray /etc/nginx/sites-available/v2ray  # copy nginx config for v2ray
+    echo "[Info] Please fill your domain in nginx config ..."
+    sudo vim /etc/nginx/sites-available/v2ray  # update nginx config file
+    echo "[Info] Linking nginx configs ..."
+    sudo ln -s /etc/nginx/sites-available/v2ray /etc/nginx/sites-enabled/
+    echo "[Info] Testing Nginx configs ..."
+    sudo nginx -t
+    echo "[Info] Change dir to ~"
+    cd ~ || exit
+    echo "[Info] Downloading v2ray & bbr shells"
+    curl -s -L https://git.io/v2ray.sh > v2ray-233boy.sh
+    curl -Lso- https://git.io/kernel.sh > v2ray-bbr.sh
+    curl -Lso- https://git.io/kernel.sh > v2ray-kernel.sh
+    echo "[Warning] Please make sure your domain pointed to this IP."
+    echo "[Info] Installing v2ray ..."
+    sudo -E bash ~/v2ray-233boy.sh
 fi
