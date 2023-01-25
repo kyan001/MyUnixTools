3cfa06065c467ab3ba4da443b49363e4fd77bba9
Kyan <ykyankai@gmail.com>
2023-01-26 01:55:25 +0800

    Update V2rayFreshStart.sh

--- V2rayFreshStart.sh
+++ V2rayFreshStart.sh
@@ -8,23 +8,23 @@ if [ "$USER" == "root" ]; then
     git clone https://github.com/kyan001/MyUnixTools  # Clone Git Repo under /root/
     cd MyUnixTools || exit  # /root/MyUnixTools, if failed exit.
     echo "[Info] Creating unix user ..."
-    sh AutoCreateUser.sh -user $username  # Enter password manually
+    sh AutoCreateUser.sh -user "$username"  # Enter password manually
     echo "[Info] Initializing zsh ..."
-    touch /home/$username/.zshrc  # init zsh
+    touch "/home/$username/.zshrc"  # init zsh
     echo "[Info] Set zsh as user default shell"
-    chsh -s /bin/zsh $username  # set zsh as user's shell
+    chsh -s /bin/zsh "$username"  # set zsh as user's shell
     echo "[Warning] You can now relogin using $username"
     exit
 else
-    # init (p10k config)
     echo "[Info] Cloning MyUnixTools ..."
     git clone https://github.com/kyan001/MyUnixTools  # Clone Git Repo under /home/$username/
     cd MyUnixTools || exit  # ~/MyUnixTools, if failed exit.
-    echo "[Info] Setup zsh ..."
+    echo "[Info] Setting up zsh ..."
     python3 AutoSetupZsh.py
-    echo "[Info] Update user configs ..."
+    echo "[Info] Updating user configs ..."
     python3 UpdateConfig.py
-    echo "[Info] Copy nginx v2ray config file ..."
+    echo "[Info] Configing p10k ... (p10k config)"
+    echo "[Info] Copying nginx v2ray config file ..."
     sudo cp config.nginx_v2ray /etc/nginx/sites-available/v2ray  # copy nginx config for v2ray
     echo "[Info] Please fill your domain in nginx config ..."
     sudo vim /etc/nginx/sites-available/v2ray  # update nginx config file
@@ -35,10 +35,15 @@ else
     echo "[Info] Change dir to ~"
     cd ~ || exit
     echo "[Info] Downloading v2ray & bbr shells"
-    curl -s -L https://git.io/v2ray.sh > v2ray-233boy.sh
-    curl -Lso- https://git.io/kernel.sh > v2ray-bbr.sh
-    curl -Lso- https://git.io/kernel.sh > v2ray-kernel.sh
+    curl -s -L https://git.io/v2ray.sh > v2ray-233boy.sh  # v2ray setup script
+    curl -Lso- https://git.io/kernel.sh > v2ray-bbr.sh  # BBR setup script
     echo "[Warning] Please make sure your domain pointed to this IP."
     echo "[Info] Installing v2ray ..."
-    sudo -E bash ~/v2ray-233boy.sh
+    sudo -E bash ~/v2ray-233boy.sh  # WebSocket + TLS
+    echo "[Info] Enabling BBR Plus ..."
+    sudo -E bash ~/v2ray-bbr.sh  # select "(1) FQ"
+    echo "[Info] Getting HTTPS ceritification ..."
+    sudo certbot --nginx  # Choose Your Domain
+    echo "[Info] Installing v2ray ..."
+    echo "[Warning] Done!"
 fi
