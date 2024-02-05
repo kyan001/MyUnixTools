32e05792c3f2640a196f3988c8f8417f167a0b29
Kyan <ykyankai@gmail.com>
2024-02-06 00:11:59 +0800

    Update pprint.sh

--- pprint.sh
+++ pprint.sh
@@ -14,67 +14,66 @@
 #
 function pprint {
     local prefix=""
-    local style=""
-    local param=""
+    local rich_style=""
+    local ansi_style=""
+    local rich_param=""
     case $1 in
         -i | --info)
             prefix="(Info) "
-            style="yellow"
-            ansi="\e[33m"
+            rich_style="yellow"
+            ansi_style="\e[33m"  # yellow
             shift
             ;;
         -w | --warn)
             prefix="(Warning) "
-            style="red"
-            ansi="\e[31m"
+            rich_style="red"
+            ansi_style="\e[31m"  # red
             shift
             ;;
         -e | --err)
             prefix="(Error) "
-            style="on red"
-            ansi="\e[41m"
+            rich_style="on red"
+            ansi_style="\e[41m"  # red background
             shift
             ;;
         -t | --title)
-            param="--rule"
-            ansi="\e[1m"
+            rich_param="--rule"
+            ansi_style="\e[1m"  # bold
             shift
             ;;
         -p | --panel)
-            param="--panel rounded"
+            rich_param="--panel rounded"
+            ansi_style="\e[4m"  # underline
             shift
             ;;
         *)
-            style="white on black"
+            rich_style="white on black"
+            ansi_style="\e[0m"  # reset
             ;;
     esac
     if command -v rich >& /dev/null; then
         local content="${*}"
-        if [[ -n $style ]]; then
-            content="[${style}]$content"
+        if [[ -n $rich_style ]]; then
+            content="[${rich_style}]${content}"
         fi
         if [[ -n $prefix ]]; then
-            content="[dim]${prefix}[/]$content"
+            content="[dim]${prefix}[/]${content}"
         fi
-        if [[ -n $param ]]; then
-            rich --print "$content" $param
+        if [[ -n $rich_param ]]; then
+            rich --print "${content} $rich_param"
         else
-            rich --print "$content"
+            rich --print "${content}"
         fi
     else
         local content="${*}"
-        local ansi_reset="\e[0m"
-        local ansi_dim="\e[2m"
+        local ansi_reset="\e[0m"  # reset
+        local ansi_dim="\e[2m"  # gray
         if [[ -n $ansi ]]; then
-            content="${ansi}$content${ansi_reset}"
+            content="${ansi}${content}${ansi_reset}"
         fi
         if [[ -n $prefix ]]; then
-            content="${ansi_dim}${prefix}${ansi_reset}$content"
+            content="${ansi_dim}${prefix}${ansi_reset}${content}"
         fi
-        if [[ -n $param ]]; then
-            builtin echo "$content" $param
-        else
-            builtin echo "$content"
-        fi
+        builtin echo "${content}"
     fi
 }
