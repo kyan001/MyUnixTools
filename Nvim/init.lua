8a76fa9cb4bf4f0f9bae166f2090349231b67594
Kyan <ykyankai@gmail.com>
2024-07-03 00:06:24 +0800

    Update init.lua

--- Nvim/init.lua
+++ Nvim/init.lua
@@ -1,8 +1,8 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -14,133 +14,155 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  default = {
-    version = "*",  -- Use the latest version of the plugins
-    lazy = true,  -- Lazy load plugins by default
-  },
-  spec = {  -- add your plugins here
-
-    -- One Half: Theme
-    {
-      "sonph/onehalf",  -- https://github.com/sonph/onehalf
-      lazy = false, -- Load this at startup
-      priority = 1000, -- Load this first
-      config = function()  -- Load the colorscheme here
-        local rtp = vim.fn.stdpath('data') .. '/lazy/onehalf/vim'  -- Path of the color scheme subfolder in the project folder
-        vim.opt.rtp:append(rtp)  -- Add the path to Run-Time Path
-        vim.cmd([[colorscheme onehalfdark]])  -- Apply the color scheme
-      end,
-    },
-
-    -- Treesitter: Syntax highlighting
-    {
-      "nvim-treesitter/nvim-treesitter",  -- https://github.com/nvim-treesitter/nvim-treesitter
-      lazy = true,
-      event = "BufRead",
-      build = ":TSUpdate",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          ensure_installed = {"bash", "c", "cpp", "css", "diff", "html", "ini", "java", "javascript", "jsdoc", "json", "jsonc", "go", "lua", "luadoc", "luap", "markdown", "markdown_inline", "php", "printf", "python", "query", "regex", "rust", "toml", "tsx", "typescript", "vim", "vimdoc", "xml", "yaml",},
-          highlight = { enable = true },
-          indent = { enable = true },
-        })
-      end,
-    },
-
-    -- nvim-notify: Better notifications
-    {
-      "rcarriga/nvim-notify",  -- https://github.com/rcarriga/nvim-notify
-      config = function()
-        vim.notify = require("notify")  -- Replace the default `vim.notify` with the one from `nvim-notify`
-      end,
-    },
-
-    -- nui.nvim: UI window frames
-    {
-      "MunifTanjim/nui.nvim",  -- https://github.com/MunifTanjim/nui.nvim
-      lazy = true,
-    },
-
-    -- noice.nvim: UI preset
-    {
-      "folke/noice.nvim",  -- https://github.com/folke/noice.nvim
-      event = "VeryLazy",  -- Load this after Nvim has loaded.
-      dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"},
-      opts = {
-        presets = {
-          bottom_search = true,  -- Enable the bottom cmdline for search
-          command_palette = false,  -- Position the cmdline and popupmenu together or not
-          long_message_to_split = true,  -- Split long messages
+    ui = {
+        icons = {
+            cmd = ">_",
+            config = "⚙",
+            event = "☇",
+            favorite = "★",
+            ft = "⎗",
+            init = "⚙",
+            import = "⎘",
+            keys = "⌨",
+            lazy = "ᶻzZ",
+            loaded = "●",
+            not_loaded = "○",
+            plugin = "❒",
+            runtime = "⏲",
+            require = "➲",
+            source = "</>",
+            start = "|>",
+            task = "✔",
+            list = { "●", "➜", "★", "‒", },
         },
-        cmdline = {
-          format = {
-            cmdline = {icon = ":"},  -- set the icon for cmdline to `:`
-            search_down = {icon = "/"},  -- set the icon for search_down to `/`
-            search_up = {icon = "?"},  -- set the icon for search_up to `?`
-            filter = {icon = ":!"},  -- set the icon for filter to `!`
-            lua = {icon = ":lua"},  -- set the icon for lua to `:lua`
-            help = {icon = ":help"},  -- set the icon for help to `:help`
-            input = {icon = ">"},  -- set the icon for input to `>>`
-          },
-        },
-      },
     },
+    default = {
+        version = "*",  -- Use the latest version of the plugins
+        lazy = true,  -- Lazy load plugins by default
+    },
+    spec = {  -- add your plugins here
 
-    -- bufferline.nvim: Show tabs at the top
-    {
-      "akinsho/bufferline.nvim",  -- https://github.com/akinsho/bufferline.nvim
-      opts = {
-        options = {
-          close_icon = "⛌",
-          buffer_close_icon = "⛌",
-          left_trunc_marker = "◀",
-          right_trunc_marker = "▶",
-          show_buffer_icons = false,  -- Disable buffer icons
-          separator_style = {"▜▛","▜▛"},
-          offsets = {
-            {
-              filetype = "neo-tree",
-              text = "Neo-tree",
-              highlight = "Directory",
-              text_align = "left",
+        -- One Half: Theme
+        {
+            "sonph/onehalf",  -- https://github.com/sonph/onehalf
+            lazy = false,  -- Load this at startup
+            priority = 1000,  -- Load this first
+            config = function()  -- Load the colorscheme here
+                local rtp = vim.fn.stdpath('data') ..
+                '/lazy/onehalf/vim'  -- Path of the color scheme subfolder in the project folder
+                vim.opt.rtp:append(rtp)  -- Add the path to Run-Time Path
+                vim.cmd([[colorscheme onehalfdark]])  -- Apply the color scheme
+            end,
+        },
+
+        -- Treesitter: Syntax highlighting
+        {
+            "nvim-treesitter/nvim-treesitter",  -- https://github.com/nvim-treesitter/nvim-treesitter
+            lazy = true,
+            event = "BufRead",
+            build = ":TSUpdate",
+            config = function()
+                require("nvim-treesitter.configs").setup({
+                    ensure_installed = { "bash", "c", "cpp", "css", "diff", "html", "ini", "java", "javascript", "jsdoc", "json", "jsonc", "go", "lua", "luadoc", "luap", "markdown", "markdown_inline", "php", "printf", "python", "query", "regex", "rust", "toml", "tsx", "typescript", "vim", "vimdoc", "xml", "yaml", },
+                    highlight = { enable = true },
+                    indent = { enable = true },
+                })
+            end,
+        },
+
+        -- nvim-notify: Better notifications
+        {
+            "rcarriga/nvim-notify",  -- https://github.com/rcarriga/nvim-notify
+            config = function()
+                vim.notify = require("notify")  -- Replace the default `vim.notify` with the one from `nvim-notify`
+            end,
+        },
+
+        -- nui.nvim: UI window frames
+        {
+            "MunifTanjim/nui.nvim",  -- https://github.com/MunifTanjim/nui.nvim
+            lazy = true,
+        },
+
+        -- noice.nvim: UI preset
+        {
+            "folke/noice.nvim",  -- https://github.com/folke/noice.nvim
+            event = "VeryLazy",  -- Load this after Nvim has loaded.
+            dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+            opts = {
+                presets = {
+                    bottom_search = true,  -- Enable the bottom cmdline for search
+                    command_palette = false,  -- Position the cmdline and popupmenu together or not
+                    long_message_to_split = true,  -- Split long messages
+                },
+                cmdline = {
+                    format = {
+                        cmdline = { icon = ":" },  -- set the icon for cmdline to `:`
+                        search_down = { icon = "/" },  -- set the icon for search_down to `/`
+                        search_up = { icon = "?" },  -- set the icon for search_up to `?`
+                        filter = { icon = ":!" },  -- set the icon for filter to `!`
+                        lua = { icon = ":lua" },  -- set the icon for lua to `:lua`
+                        help = { icon = ":help" },  -- set the icon for help to `:help`
+                        input = { icon = ">" },  -- set the icon for input to `>>`
+                    },
+                },
             },
-          },
         },
-      }
-      -- dependencies = {"nvim-tree/nvim-web-devicons"},
-    },
 
-    -- nvim-lualine: Status line
-    {
-      "nvim-lualine/lualine.nvim",  -- https://github.com/nvim-lualine/lualine.nvim
-      opts = {
-        options = {
-          icons_enabled = false,  -- Disable icons
+        -- bufferline.nvim: Show tabs at the top
+        {
+            "akinsho/bufferline.nvim",  -- https://github.com/akinsho/bufferline.nvim
+            -- dependencies = {"nvim-tree/nvim-web-devicons"},
+            opts = {
+                options = {
+                    close_icon = "⛌",
+                    buffer_close_icon = "⛌",
+                    left_trunc_marker = "◀",
+                    right_trunc_marker = "▶",
+                    show_buffer_icons = false,  -- Disable buffer icons (need `nvim-web-devicons` and Nerd Font)
+                    offsets = {
+                        {
+                            filetype = "neo-tree",
+                            text = "Neo-tree",
+                            highlight = "Directory",
+                            text_align = "left",
+                        },
+                    },
+                },
+            },
         },
-      },
-      -- dependencies = {"nvim-tree/nvim-web-devicons"},
-    },
 
-    -- indent-blankline.nvim: Show indent lines
-    {
-      "lukas-reineke/indent-blankline.nvim",  -- https://github.com/lukas-reineke/indent-blankline.nvim
-      main = "ibl",
-      opts = {},
-    },
+        -- nvim-lualine: Status line
+        {
+            "nvim-lualine/lualine.nvim",  -- https://github.com/nvim-lualine/lualine.nvim
+            -- dependencies = {"nvim-tree/nvim-web-devicons"},
+            opts = {
+                options = {
+                    icons_enabled = false,  -- Disable icons (need `nvim-web-icons` and Nerd Font)
+                },
+            },
+        },
 
-    -- neo-tree
-    {
-      "nvim-neo-tree/neo-tree.nvim",  -- https://github.com/nvim-neo-tree/neo-tree.nvim
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        -- "nvim-tree/nvim-web-devicons",
-      },
-    },
-  },  -- $spec
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+        -- indent-blankline.nvim: Show indent lines
+        {
+            "lukas-reineke/indent-blankline.nvim",  -- https://github.com/lukas-reineke/indent-blankline.nvim
+            main = "ibl",
+            opts = {},
+        },
+
+        -- neo-tree
+        {
+            "nvim-neo-tree/neo-tree.nvim",  -- https://github.com/nvim-neo-tree/neo-tree.nvim
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                -- "nvim-tree/nvim-web-devicons",
+                "MunifTanjim/nui.nvim",
+            },
+        },
+    },  -- $spec
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "habamax" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
 })
